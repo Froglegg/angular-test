54e094bfaa4b45831fa960bbad3cd9933c95bf57
froglegg <hayescrowley@gmail.com>
2020-01-08 15:52:05 -0500

    first push

---
+++ client/src/app/express-server-component/express-server-component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "express-server",
+  templateUrl: "express-server.component.html"
+})
+export class ExpressServer implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
